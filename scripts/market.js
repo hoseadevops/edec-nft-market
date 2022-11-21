f7b06750f90773ff4aa6293e7661367a34adf8b4
hoshaeyah <hoshaeyah@hotmail.com>
2022-11-21 10:45:20 +0800

    test

--- scripts/market.js
+++ scripts/market.js
@@ -239,11 +239,11 @@ async function main() {
     const accountsAssets = getMockTokenAsset(accounts);
     
     // submit orders
-    // await match721Order(deployed, mockDeployed, accounts, accountsAssets);
+    await match721Order(deployed, mockDeployed, accounts, accountsAssets);
     
-    // await match1155Order(deployed, mockDeployed, accounts, accountsAssets);
+    await match1155Order(deployed, mockDeployed, accounts, accountsAssets);
 
-    await match721BatchOrder(deployed, mockDeployed, accounts, accountsAssets);
+    // await match721BatchOrder(deployed, mockDeployed, accounts, accountsAssets);
 }
 
 main()
