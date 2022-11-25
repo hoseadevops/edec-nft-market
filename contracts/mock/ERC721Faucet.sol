4c68820659747bb1869e67a0ee393ed66fa369f4
hoshaeyah <hoshaeyah@hotmail.com>
2022-11-25 09:49:18 +0800

    test

---
+++ contracts/mock/ERC721Faucet.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract ERC721Faucet is ERC721 {
+    
+    using Counters for Counters.Counter;
+    
+    Counters.Counter private _tokenIds;
+
+    uint256 public constant count = 20;
+
+    uint256 public maxTokenId = 0;
+
+    constructor() ERC721("EF721", "EF") {}
+
+    function faucet() public {
+        uint tokenId = _tokenIds.current();
+        maxTokenId = tokenId + count - 1;
+        while( tokenId <= maxTokenId ) {
+            _mint(msg.sender, tokenId);
+            _tokenIds.increment();
+            tokenId = _tokenIds.current();
+        }
+    }
+}
